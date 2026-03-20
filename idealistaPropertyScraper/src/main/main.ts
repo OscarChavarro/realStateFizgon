ec834bd48a48f5d20c50202f0dc13cb02c78f017
Oscar Chavarro <jedilink@gmail.com>
2026-03-20 20:15:52 +0100

    Hexagonal architecture: man using port

--- idealistaPropertyScraper/src/main/main.ts
+++ idealistaPropertyScraper/src/main/main.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
-import { ScraperConfig } from 'infrastructure/config/settings/scraper.config';
+import { SCRAPER_SETTINGS_PORT } from 'ports/outbound/settings/scraper-settings.port.token';
+
+import type { ScraperSettingsPort } from 'ports/outbound/settings/scraper-settings.port';
 
 async function bootstrap(): Promise<void> {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  const scraperConfig = app.get(ScraperConfig);
+  const scraperConfig = app.get<ScraperSettingsPort>(SCRAPER_SETTINGS_PORT);
   await app.listen(scraperConfig.apiHttpPort, '0.0.0.0');
   logger.log(`HTTP API endpoints are available on TCP port ${scraperConfig.apiHttpPort}.`);
 }
